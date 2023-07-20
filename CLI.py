24d8e2be22cc4cdae33379f6c7ba6665ae7ced5f
ian-smithg <125311683+ian-smithg@users.noreply.github.com>
2023-07-20 14:37:27 -0600

    bug fixes

--- CLI.py
+++ CLI.py
@@ -214,7 +214,7 @@ def main():
     args = create_parser().parse_args()
     if args.start:
         print("Starting server")
-        subprocess.Popen("python3 /home/bonelab/server/bls/main_1.7.obj.py")
+        subprocess.Popen(["python3", "/home/bonelab/server/bls/main_1.7.obj.py"])
     else:
         CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         CLIENT.connect(ADDR)
