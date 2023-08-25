86a8e7e37192c5cabec5299a75854924ff565d17
ian-smithg <125311683+ian-smithg@users.noreply.github.com>
2023-08-24 22:33:04 -0600

    Processing error handling

--- send.py
+++ send.py
@@ -81,9 +81,6 @@ class Send:
     def _send_radius_tibia_final(self):
         sftp_cmd = ['sftp', '{}@{}:{}'.format(self.username, self.hostname, ip_utils.convert_path(self.destination))]
         masks = ip_utils.get_abs_paths(self.image_dir)
-        client_destination = ip_utils.convert_path(self.destination)
-        print(masks[0])
-        print(masks[1])
         self.logs.log_debug("Sending {} and {}".format(masks[0], masks[1]))
         put_cmd1 = ['put ' + masks[0], "quit"]
         put_cmd2 = ['put ' + masks[1], "quit"]
