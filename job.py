607106328b080115be911f6e691fb1f74395f334
ian.smith2 <ian.smith2@ucalgary.ca>
2024-01-11 15:13:34 -0700

    Problem with getting data from yaml

--- job.py
+++ job.py
@@ -101,10 +101,9 @@ class JobData:
         raise FileNotFoundError
 
     def _parse_yaml(self):
-        print(self.com_file_path)
         with open(self.com_file_path, 'r') as file:
             command_file = yaml.safe_load(file)
-        return command_file
+            return command_file
 
     def _write_yaml(self):
         with open(self.com_file_path, 'w') as file:
